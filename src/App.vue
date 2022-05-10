30d446e0b40866695ed7e3a51da698abd8f0882e
Aurel1701 <gantzfan96@gmail.com>
2022-05-10 10:28:46 +0200

    edit template

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,58 @@
 <template>
   <div id="app">
-    <HeaderBox @searching="ricerca" />
-    <CardsBox :movies="movies" />
+    <header class="container-fluid d-flex header">
+      <div class="container d-flex">
+        <div class="logo-box d-flex">
+          <img src="./assets/logo.png" alt="" />
+        </div>
+        <input
+          v-model="keySearch"
+          type="text"
+          class="form-control mt-3 border rounded"
+          placeholder="Cerca un film"
+          aria-label="Search"
+        />
+        <button
+          type="button"
+          class="btn text-light rounded"
+        >
+          Cerca
+        </button>
+      </div>
+    </header>
+
+    <!-- ricerca film -->
+
+    <div>
+      <div>
+        <h4 class="text-light text-center">Films:</h4>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <cards-box
+          v-for="movie in movies"
+          :key="movie.id"
+          :info="movie"
+          :type="'movie'"
+        />
+        <img id="" :src="``">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HeaderBox from "./components/HeaderBox.vue";
-import CardsBox from "./components/CardsBox.vue";
 import axios from "axios";
 export default {
   name: "App",
   components: {
-    HeaderBox,
-    CardsBox,
+
   },
   data() {
     return {
       movies: [], // array film  da riempire tramite la chiamata con i film salvati
       api_key: "5ad2455a12cd749ceea122cfdf1861d7",
+      keySearch: "",
     };
   },
   methods: {
@@ -28,12 +62,15 @@ export default {
         api_key: this.api_key,
       };
       return axios
-        .get(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=5ad2455a12cd749ceea122cfdf1861d7&language=en-US`, { params })
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=5ad2455a12cd749ceea122cfdf1861d7&query=ritorno+al+fut`,
+          { params }
+        )
         .then((response) => {
-          this.movies = response.data.results; 
+          this.movies = response.data.results;
         });
     },
-    ricerca(query){
+    ricerca(query) {
       this.searchMov(query);
     },
   },
